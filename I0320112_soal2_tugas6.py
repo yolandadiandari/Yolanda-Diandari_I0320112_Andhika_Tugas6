55acc3750736017b944f1b3a6cbcad018d1a19e2
Yolanda Diandari <yolandadiandari@gmail.com>
2021-04-08 22:36:37 +0700

    update tugas 6

--- I0320112_soal2_tugas6.py
+++ I0320112_soal2_tugas6.py
@@ -1,1 +1,14 @@
 
+#masukkan jumlah data
+x = int(input('Masukkan jumlah data nilai:'))
+
+nilai = []
+jumlah = 0
+
+#program
+for i in range(0, x):
+    list_nilai = int(input("Nilai siswa %d :" % (i + 1)))
+    nilai.append(list_nilai)
+    jumlah = jumlah + nilai[i]
+    rata_rata = jumlah / x
+print("Rata-rata nilai siswa adalah = %0.3f" % rata_rata)
