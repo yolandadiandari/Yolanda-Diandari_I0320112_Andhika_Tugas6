68d3d1ebf90ab63ce1058a1f57c9dd14c2e5c961
Yolanda Diandari <yolandadiandari@gmail.com>
2021-04-08 22:39:27 +0700

    update tugas 6

--- I0320112_soal3_tugas6.py
+++ I0320112_soal3_tugas6.py
@@ -1,1 +1,13 @@
+for n in range(10, 25):
+    if n == 1:
+        print(n, "bukan prima")
+    elif n == 2:
+        print(n, "adalah bilangan prima")
+    else:
+        for i in range(2, n):
+            if n % i == 0:
+                print(n, "bukan prima")
+                break
+        else:
+            print(n, "adalah bilangan prima")
 
